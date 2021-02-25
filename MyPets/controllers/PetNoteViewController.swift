dae5e3cb83cc353c9247cf7f5d3a50982bdf3b28
nurymsiyrbayev <nurym.kazah@gmail.com>
2021-02-25 22:52:57 +0600

    Aded PetViewController,PetNoteViewController,AddPetViewController,PetsCollectionViewCell

---
+++ MyPets/controllers/PetNoteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PetNoteViewController.swift
+//  MyPets
+//
+//  Created by ADMIN ODoYal on 25.02.2021.
+//
+
+import UIKit
+
+class PetNoteViewController: UIViewController {
+
+    @IBOutlet weak var PetNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
